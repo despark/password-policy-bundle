5b78f1dfa4fad60a3dc984859a3637cc59510783
Ludmil Simeonov <lsimeonov@despark.com>
2018-07-13 16:52:21 +0300

    Change password policy error copy

--- src/Validator/PasswordPolicy.php
+++ src/Validator/PasswordPolicy.php
@@ -15,5 +15,5 @@ class PasswordPolicy extends Constraint
 {
     const PASSWORD_IN_HISTORY = '72a1be03-a5d1-4b23-bd70-a6841992a03c';
 
-    public $message = 'You changed this password {{ days }}';
+    public $message = 'Cannot change your password to an old one. You used this password {{ days }}';
 }
